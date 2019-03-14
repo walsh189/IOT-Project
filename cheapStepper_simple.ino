cdd513fbfe4c34dabc2a3e234097ab63e7ac32cf
walsh189 <48416727+walsh189@users.noreply.github.com>
2019-03-14 16:15:44 +0000

    Add files via upload

--- cheapStepper_simple.ino
+++ cheapStepper_simple.ino
@@ -1,5 +1,5 @@
 /*
- *///////////////////////////////////////////////////////
+ * //////////////////////////////////////////////////////
  * Internet of Things Project
  * Aaron Walsh, Robert Moore, Olujide Egberongbe
  * //////////////////////////////////////////////////////
@@ -30,56 +30,55 @@ int buttonStatus = 0;
 
 void setup() 
 {
-	//set up a serial connection and test print to the console
-	Serial.begin(9600);
+  //set up a serial connection and test print to the console
+  Serial.begin(9600);
   
-	//set the PIN and begin blynk
-	pinMode(3, INPUT);
-	Blynk.begin(auth);
+  //set the PIN and begin blynk
+  pinMode(3, INPUT);
+  Blynk.begin(auth);
 }
 
 void loop() {
-	Blynk.run();
+  Blynk.run();
   
-	//button status reads in PIN
-	buttonStatus = digitalRead(3);
+  //button status reads in PIN
+  buttonStatus = digitalRead(3);
   
   
-	if (buttonStatus == HIGH)
-	{
-		for(int x=0; x<2; x++)
-		{
-			for (int s=0; s<2048; s++)
-			{
-				// 4096 steps = full rotation using default values
-				// this will loop 2048 times for a half rotation
+  if (buttonStatus == HIGH)
+  {
+    for(int x=0; x<2; x++)
+    {
+      for (int s=0; s<2048; s++)
+      {
+        // 4096 steps = full rotation using default values
+        // this will loop 2048 times for a half rotation
 
-				// let's move one "step" (of the 2048 per full rotation)
-				stepper.step(moveClockwise);
-				
-				/* the direction is based on moveClockwise boolean:
-				* true for clockwise, false for counter-clockwise
-				*/
+        // let's move one "step" (of the 2048 per full rotation)
+        stepper.step(moveClockwise);
+        
+        /* the direction is based on moveClockwise boolean:
+        * true for clockwise, false for counter-clockwise
+        */
 
-				//get the current step position of motor
-				int nStep = stepper.getStep();
+        //get the current step position of motor
+        int nStep = stepper.getStep();
 
-				//and if it's divisible by 64...
-				if (nStep%64==0)
-				{ 
-					//let's print the position to the console
+        //and if it's divisible by 64...
+        if (nStep%64==0)
+        { 
+          //let's print the position to the console
       
-					Serial.print("current step position: "); Serial.print(nStep);
-					Serial.println();
-				}
-			}
-			//short delay
-			delay(1000);
+          Serial.print("current step position: "); Serial.print(nStep);
+          Serial.println();
+        }
+      }
+      //short delay
+      delay(1000);
 
-			//and switch directions for one more rotation
-			moveClockwise = !moveClockwise;
-		}
-	} 
+      //and switch directions for one more rotation
+      moveClockwise = !moveClockwise;
+    }
+  } 
 }
-
 
